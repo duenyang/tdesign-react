dd5fd38e33ce11c3510b8a1a2b64c18da0513c2c
duenyang <377153400@qq.com>
2022-01-23 14:24:11 +0800

    chore(textarea): 增加textarea单测

--- src/textarea/__tests__/textarea.test.tsx
+++ src/textarea/__tests__/textarea.test.tsx
@@ -1,4 +1,17 @@
-import { testExamples } from '@test/utils';
+import React from 'react';
+import { testExamples, render } from '@test/utils';
+import Textarea from '../Textarea';
 
 // 测试组件代码 Example 快照
 testExamples(__dirname);
+
+const textareaText = 'textarea test';
+
+describe('Textarea', () => {
+  test('render correct width value', () => {
+    const { container, asFragment } = render(<Textarea value={textareaText}></Textarea>);
+    expect(container.firstChild.classList.contains('t-textarea')).toBeTruthy();
+    expect(container.querySelector('textarea').value).toEqual(textareaText);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
